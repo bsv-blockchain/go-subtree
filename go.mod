d3fc1c3e05fecb124376af5ec1f3faeabb693690
Dylan Murray <dylan@britevue.com>
2025-07-01 00:26:40 -0400

    Add go-subtree source code

--- go.mod
+++ go.mod
@@ -9,3 +9,5 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
+
+replace github.com/libsv/go-bt/v2 => github.com/ordishs/go-bt/v2 v2.2.22
---
+++ subtree_benchmark_test.go
@@ -0,0 +1,61 @@
+package subtree_test
+
+func BenchmarkSubtree_AddNode(b *testing.B) {
+	st, err := NewIncompleteTreeByLeafCount(b.N)
+	require.NoError(b, err)
+
+	// create a slice of random hashes
+	hashes := make([]chainhash.Hash, b.N)
+
+	b32 := make([]byte, 32)
+
+	for i := 0; i < b.N; i++ {
+		// create random 32 bytes
+		_, _ = rand.Read(b32)
+		hashes[i] = chainhash.Hash(b32)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_ = st.AddNode(hashes[i], 111, 0)
+	}
+}
+
+func BenchmarkSubtree_Serialize(b *testing.B) {
+	st, err := NewIncompleteTreeByLeafCount(b.N)
+	require.NoError(b, err)
+
+	for i := 0; i < b.N; i++ {
+		// int to bytes
+		var bb [32]byte
+
+		binary.LittleEndian.PutUint32(bb[:], uint32(i))
+		_ = st.AddNode(*(*chainhash.Hash)(&bb), 111, 234)
+	}
+
+	b.ResetTimer()
+
+	ser, err := st.Serialize()
+	require.NoError(b, err)
+	assert.GreaterOrEqual(b, len(ser), 48*b.N)
+}
+
+func BenchmarkSubtree_SerializeNodes(b *testing.B) {
+	st, err := NewIncompleteTreeByLeafCount(b.N)
+	require.NoError(b, err)
+
+	for i := 0; i < b.N; i++ {
+		// int to bytes
+		var bb [32]byte
+
+		binary.LittleEndian.PutUint32(bb[:], uint32(i))
+		_ = st.AddNode(*(*chainhash.Hash)(&bb), 111, 234)
+	}
+
+	b.ResetTimer()
+
+	ser, err := st.SerializeNodes()
+	require.NoError(b, err)
+	assert.GreaterOrEqual(b, len(ser), 32*b.N)
+}
